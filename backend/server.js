df478db17a034e9e53ff7a5f31e211703c1d4c5f
Fadia <alatrech.fadya09@gmail.com>
2023-11-16 09:27:13 +0100

    deploy 11

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use("/assets", express.static(path.join(__dirname, "src/assets")));
 
 if (process.env.NODE_ENV === "production") {
   app.use("/uploads", express.static("/var/data/uploads"));
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
