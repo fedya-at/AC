d685dddd00b19dd00debd93fc69b2f025e6b7cdb
Fadia <alatrech.fadya09@gmail.com>
2023-11-24 09:28:22 +0100

    ssk

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://ac-photovoltaic.onrender.com",
         changeOrigin: true,
       },
     },
